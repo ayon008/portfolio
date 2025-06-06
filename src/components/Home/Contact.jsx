f6dba5fe4672bde849af79817461a26cfd87f279
ayon008 <shariar.ayon128@gmail.com>
2025-06-06 12:09:07 +0600

    a

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import montserrat from '../fonts/Monserrat';
 import Title from '../Shared/Title/Title';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+import { Button } from '../ui/button';
 
 const CDiv = ({ text, value }) => {
     return (
@@ -20,7 +23,23 @@ const Contact = () => {
                 <CDiv text={'Website'} value={'https://portfolio-lake-five-93.vercel.app/'} />
             </div>
             <div>
-                <Title header={'If you have any porject or need help. Contact me'} subheader={'If you have any porject or need help. Contact me'}/>
+                <Title header={'If you have any porject or need help. Contact me'} subheader={'If you have any porject or need help. Contact me'} />
+                <form className='space-y-8 mt-10'>
+                    <div className='flex items-center gap-6'>
+                        <Input type="name" className={'rounded-none border border-[#3A3C48] py-8 px-6'} placeholder="Name" />
+                        <Input type="email" className={'rounded-none border border-[#3A3C48] py-8 px-6'} placeholder="Email" />
+                    </div>
+                    <div className='flex items-center gap-6'>
+                        <Input type="phone" className={'rounded-none border border-[#3A3C48] py-8 px-6'} placeholder="Phone" />
+                        <Input type="text" className={'rounded-none border border-[#3A3C48] py-8 px-6'} placeholder="Subject" />
+                    </div>
+                    <Textarea className={'rounded-none border border-[#3A3C48] py-20 px-6'} placeholder="Type your message here." />
+                    <Button className="border border-[#3A3C48] bg-[#252734] hover:bg-[#2A2C39] transition-all duration-300 hover:border-[#2A2C39] py-7 rounded-4xl text-lg cursor-pointer">
+                        <span className='flex items-center gap-2 px-6'>
+                            <span className={`${montserrat.className}`}>Submit Message</span>
+                        </span>
+                    </Button>
+                </form>
             </div>
         </div>
     );
