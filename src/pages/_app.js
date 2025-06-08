b4a227f87abf6eb1972948cd610a13546a191536
ayon008 <shariar.ayon128@gmail.com>
2025-06-09 00:01:34 +0600

    a

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
+import Head from "next/head";
 import Main from "@/components/Layouts/Main";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Main>
-      <Component {...pageProps} />
-    </Main>
+    <>
+      <Head>
+        <title>Shariar Ayon - Wordpress and Full Stack Developer</title>
+        <meta name="description" content="Default description for all pages" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Main>
+        <Component {...pageProps} />
+      </Main>
+    </>
   );
 }
